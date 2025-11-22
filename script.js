fb61b9f8602cca185d268169c7701e8bef4d959c
Prem Kumar <prem144047@gmail.com>
2025-11-22 16:53:27 +0000

    Enhance script.js with additional functions and logging for array manipulation

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((person) => {
+    if (person.profession === "developer") {
+      console.log(person);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+   arr.forEach((person) => {
+    if (person.profession === "developer") {
+      console.log(person);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "Prem Kumar", age: "25", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filteredArr = arr.filter((person) => person.profession !== "admin");
+  console.log(filteredArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 5, name: "mark", age: "25", profession: "developer" },
+    { id: 6, name: "emma", age: "23", profession: "admin" },
+  ];
+
+  const combined = arr.concat(newArr);
+  console.log(combined);
 }
